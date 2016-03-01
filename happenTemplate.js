84e37a86466e0901e1c000601e320500bb9650a8
LaMonte <austinlamonte@yahoo.com>
2016-03-01 14:55:02 -0600

    initial commit

--- happenTemplate.js
+++ happenTemplate.js
@@ -48,10 +48,26 @@ var library = (function() {
 	})(),
 	Week: (function(){
 		return {
-			DayOfWeek: function(){},
-			AbrDayOfWeek: function(){},
-			FirstTwoOfWeek: function(){},
-			WeekOfYear: function(){}
+			DayOfWeek: function(){
+                var date = new Date();
+                var weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+                return weekDay[date.getDay()-1];   
+            },
+			AbrDayOfWeek: function(){
+                var date = new Date();
+                var weekDay = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+                return weekDay[date.getDay()-1];
+            },
+			FirstTwoOfWeek: function(){
+                var date = new Date();
+                var weekDay = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+                return weekDay[date.getDay()-1];
+            },
+			WeekOfYear: function(){
+                var date = new Date();
+                var weekYear = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31","32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52"];
+                return weekYear[date.getDate()+7];
+            }
 		}
 	})(),
 	Month: (function(){
@@ -101,12 +117,51 @@ var library = (function() {
 		return {
 			DayOfYear: (function(){
 				return {
-					Numeral: function(){},
-					Ordinal: function(){}
+					Numeral: function(){
+                        var now = new Date();
+                        var start = new Date(now.getFullYear(), 0, 0);
+                        var diff = now - start;
+                        var oneDay = 1000 * 60 * 60 * 24;
+                        var day = Math.floor(diff / oneDay);
+                      
+                        return day.toString();
+                    },
+					Ordinal: function(){
+                        var now = new Date();
+                        var start = new Date(now.getFullYear(), 0, 0);
+                        var diff = now - start;
+                        var oneDay = 1000 * 60 * 60 * 24;
+                        var day = Math.floor(diff / oneDay);    
+                        var ordinal = function(day) {     
+                            var j = day % 10,
+                                k = day  % 100;
+                            if (j == 1 && k != 11) {
+                                return day + "st";
+                            }
+                            if (j == 2 && k != 12) {
+                                return day + "nd";
+                            }
+                            if (j == 3 && k != 13) {
+                                return day + "rd";
+                            } 
+                            return day + "th";
+                        };
+                        
+                        return ordinal(day).toString();
+                    }
 				}
 			})(),
-			YearFull: function(){},
-			YearAbr: function(){}
+			YearFull: function(){
+                var date = new Date();
+                var year = date.getFullYear(); 
+                return String(year);
+                
+            },
+			YearAbr: function(){
+                var date = new Date();
+                var year = String(new Date().getFullYear()); 
+                return year.substring(2,4);
+            }
 		}
 	})(),
 	Defaults: function(){}
